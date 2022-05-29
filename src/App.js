70795a660ee4dc2c4f6f909af1ef68918d32454a
faisalk2 <faisal.z.khan5538@gmail.com>
2022-05-29 13:31:12 +0530

    navbar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./components/Hello"
+import Answer1  from './components/Answer1';
+import React from "react";
+
+
+import WithSubnaviga from "./components/WithSubnavigation";
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+  <WithSubnavigation/>
+
+
+    </div>
+  );
+}
+
+export default App;
